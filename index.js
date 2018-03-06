ba28302064f63a7c1c48202f28580aacde589c25
robert80203 <robert80203@gmail.com>
2018-03-06 21:25:44 +0800

    2018/03/06 1.remove proceeding 2.add keynotes 3.change agenda

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function go_agenda(){
 	<div class="left_ele title_css">
 		Symposium Agenda
 	</div>`;
-	var newele2 = '<div class="left_ele" style="text-align:center"><iframe  style="margin-left:5%;" width="850px" height="1100px" src="agenda.JPG" frameborder="0"></iframe></div>';
+	var newele2 = '<div class="left_ele" style="text-align:center"><iframe  style="margin-left:5%;" width="1000px" height="1300px" src="agenda.JPG" frameborder="0"></iframe></div>';
 	parent.append(newele);
 	parent2.append(newele2);
 	
@@ -344,7 +344,12 @@ function go_keyspe(){
 	
 	var newele2 = `
 		<div class="left_ele" style="text-align:center;margin-top:3%;">
-		  <img  src="unavailable.png" alt="error" style="width:15vw;height:15vw;border:0;">
+		  <img  src="keynote1.JPG" style="width:650px;height:200px;border:0;">
+		  <img  src="keynote4.JPG" style="width:650px;height:200px;border:0;">
+		  <img  src="keynote5.JPG" style="width:650px;height:200px;border:0;">
+		  <img  src="keynote2.JPG" style="width:650px;height:200px;border:0;">
+		  <img  src="keynote3.JPG" style="width:650px;height:200px;border:0;">
+		  <img  src="keynote6.JPG" style="width:650px;height:200px;border:0;">
 		</div>
 	`;
 	parent.append(newele);
