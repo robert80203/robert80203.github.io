4f5f01b5952f3a7f1c6d6bae91297d36104a6e3d
robert80203 <robert80203@gmail.com>
2020-07-11 17:46:22 +0800

    update

--- index.js
+++ index.js
@@ -99,7 +99,6 @@ function go_lec_DLP(){
 	<div class="left_ele title_style">
 		Deep Learning and Practice
 	</div>`;
-	
 	var newele2 = `
 		<div class="left_ele ele_container">
 			<div class="title">Course Description</div>
@@ -109,13 +108,58 @@ function go_lec_DLP(){
                 and familiarize with deep learning tools, PyTorch, by working on hands-on labs.
                 Students are required to present papers and develop final projects which should be presented in a on site workshop.
 			</div>
+            <div class="title">Spring Semester 2019 - Selected Projects</div>
+            <table class="research_table">
+                <tbody>
+                    <tr>
+                        <td class="research_td">
+                            <iframe class="video" src="https://youtube.com/embed/KYVma8KVT_k" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <div class="research_title">Contextual Attention Inpainting Network with Non-local Blocks</div>
+                            <div class="research_author">劉嘉豪, 朱蝶</div>
+                            <div class="link"><a href="#">Poster</a></div>
+                            <button class="button" onclick="ShowAbstract('project_2019_1');">Abstract</button>
+                            <div class="abstract" id="project_2019_1">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="link">
+                                <a href="https://timetable.nctu.edu.tw/?r=main/crsoutline&Acy=108&Sem=2&CrsNo=5253&lang=zh-tw">Syllabus</a> |
+                                <a href="https://www.youtube.com/watch?v=O0ijvhG1Xoo&list=PLZAKTu7XAqHUukdYlwdWbZ0yj-q9TAWPA">Project Video</a>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="title">Summer Semester 2018 - Selected Projects</div>
+            <table class="research_table">
+                <tbody>
+                    <tr>
+                        <td class="research_td">
+                            <iframe class="video" src="https://youtube.com/embed/cSxyAMotcjo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <div class="research_title">Fast Semantic Segmentation on Video Using Motion Vector-Based Feature Interpolation</div>
+                            <div class="research_author">陳詒欣, 陳梅香</div>
+                            <div class="link"><a href="#">Poster</a></div>
+                            <button class="button" onclick="ShowAbstract('project_summer_2019_1');">Abstract</button>
+                            <div class="abstract" id="project_summer_2019_1">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="link">
+                                <a href="https://timetable.nctu.edu.tw/?r=main/crsoutline&Acy=108&Sem=2&CrsNo=5253&lang=zh-tw">Syllabus</a> |
+                                <a href="https://www.youtube.com/watch?v=O0ijvhG1Xoo&list=PLZAKTu7XAqHUukdYlwdWbZ0yj-q9TAWPA">Project Video</a>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
             <div class="title">Spring Semester 2018 - Selected Projects</div>
             <table class="research_table">
                 <tbody>
                     <tr>
-                        <!--<td>
-            				
-                        </td>-->
                         <td class="research_td">
                             <iframe class="video" src="https://www.youtube.com/embed/CGPo546M6d8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             <div class="research_title">Learnt Image Compressor</div>
@@ -147,8 +191,7 @@ function go_lec_DLP(){
                         <td>
                             <div class="link">
                                 <a href="https://timetable.nctu.edu.tw/?r=main/crsoutline&Acy=108&Sem=2&CrsNo=5253&lang=zh-tw">Syllabus</a> |
-                                <a href="https://www.youtube.com/watch?v=QgoxtGi-trU&list=PLZAKTu7XAqHWY1J522ldYk29DjzdGA8jh">Project Video</a> | 
-                                <a href="#">Final Workshop</a>
+                                <a href="https://www.youtube.com/watch?v=QgoxtGi-trU&list=PLZAKTu7XAqHWY1J522ldYk29DjzdGA8jh">Project Video</a>
                             </div>
                         </td>
                     </tr>
